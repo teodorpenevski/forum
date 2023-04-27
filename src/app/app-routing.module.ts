c8de906c7d5b03b2af4a7169eaf8f18331cc7efe
Matej Trpkovski <mtrpkovski@yahoo.com>
2023-04-27 12:24:02 +0200

    Made navbar prototype

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { PostComponent } from "./components/post/post.component";
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { TagsPageComponent } from './pages/tags-page/tags-page.component';
 
 const routes: Routes = [
-  { path: 'post', component: PostComponent }
+  { path: 'home', component: HomePageComponent },
+  { path: 'tags', component: TagsPageComponent },
+  { path: 'posts', component: PostComponent }
 ];
 
 @NgModule({
