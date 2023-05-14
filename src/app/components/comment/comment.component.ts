0eb0dbf928c23c9f73d32b24ccd0bbf073ed71db
keko24 <kotevski_andrej@hotmail.com>
2023-05-14 22:40:57 +0200

    Fixed problems with comment and post.

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommentService } from "../../services/comment.service";
+import { Comment } from "../../models/comment";
 
 @Component({
   selector: 'app-comment',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./comment.component.css']
 })
 export class CommentComponent {
+  @Input() comment: Comment = {
+    id: 1,
+    comment: "I love dogs!",
+    createdBy: "Jane Doe"
+  }
+  commentText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl eu nunc."
+  commentBy = 'John Doe';
+  commentDate = '2019-01-01';
+  lastEditDate = '2019-01-01';
+
+  constructor(private service: CommentService) { }
+
+  ngOnInit() {
+
+  }
 
 }
--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,10 +1,111 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { PostService } from "../../services/post.service";
+import { Post } from "../../models/post";
+import { Comment } from "../../models/comment";
+import { Tag } from "../../models/tag";
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
 
+export class PostComponent implements Post {
+  id: number = 1;
+  user: string = 'John Doe';
+  title = 'Dogs are awesome';
+  text = 'Dogs are the best pets ever. They are loyal, friendly, and cute. They are also a lot of work. You have to feed them, walk them, and clean up after them. But it is all worth it.';
+  likes = 0;
+  dislikes = 0;
+  createdBy = 'John Doe';
+  postedDate = 'March 1, 2018';
+  isSaved: boolean = false;
+  lastEditedDate = 'March 1, 2018';
+  tags: Tag[] = [
+    {
+      name: 'dogs'
+    },
+    {
+      name: 'pets'
+    },
+    {
+      name: 'animals'
+    }
+  ];
+  comments: Comment[] = [
+    {
+      id: 1,
+      comment: "I love dogs!",
+      createdBy: "Jane Doe"
+    }
+  ];
+  commentCount: number = 1;
+  sortAttribute: string = 'created';
+  ascendingOrDescending: boolean = true;
+
+  commentForm = new FormGroup({
+    text: new FormControl('', [
+      Validators.required
+    ]),
+  });
+
+  constructor(private route: ActivatedRoute,
+              private service: PostService
+  ) { }
+
+  ngOnInit() {
+    this.getPost();
+  }
+
+  getPost() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.service.getPost(id).subscribe(post => {
+      if (post) {
+        this.id = post.id;
+        this.text = post.text;
+        this.title = post.title;
+        this.likes = post.likes;
+        this.dislikes = post.dislikes;
+        this.tags = post.tags;
+        this.comments = post.comments;
+        this.commentCount = post.comments.length;
+      }
+    });
+  }
+
+  onComment() {
+    if (this.commentForm.invalid) return;
+    this.service.onComment(this.id, this.commentForm.value.text!!).subscribe({
+      complete: () => {
+        this.updateComments();
+      }
+    });
+    this.commentForm.reset();
+  }
+
+  updateComments() {
+    this.service.getPostComments(this.id).subscribe(comments => {
+      this.comments = comments;
+      this.commentCount = comments.length;
+    });
+  }
+
+  onEdit() {
+
+  }
+
+  onDelete() {
+
+  }
+
+  sort(attribute: string) {
+    if (this.sortAttribute === attribute) {
+      this.ascendingOrDescending = !this.ascendingOrDescending;
+    } else {
+      this.ascendingOrDescending = true;
+      this.sortAttribute = attribute;
+    }
+  }
 }
