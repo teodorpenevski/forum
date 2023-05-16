1399faf2bf1ff48247a007b9ef4b474e883d8cce
Matej Trpkovski <mtrpkovski@yahoo.com>
2023-05-16 23:05:49 +0200

    Style for login updated + header 'log-in' button to 'account' button if user is logged in

--- src/app/pages/nav-bar/nav-bar.component.ts
+++ src/app/pages/nav-bar/nav-bar.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Link } from 'src/app/interfaces/link';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+
+  currentUser: string;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.authService.getCurrentUser().subscribe(user => this.currentUser = user);
+  }
 
   mainNavLinks: Link[] = [
     {
